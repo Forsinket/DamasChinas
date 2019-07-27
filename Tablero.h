c397a27e37d5706ac1675c679367841b2770429b
Invitado <invitado@s1pc2.eisc.univalle.edu.co>
2019-07-26 19:19:15 -0500

    Se Creo Tablero

---
+++ Tablero.h
@@ -0,0 +1,43 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
+
+
+
+class Tablero{
+
+
+
+    private: 
+        int Filas();
+        int columnas();
+
+
+    public: 
+        Tablero();
+        ~Tablero();
+        void GenerarTablero();     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+#endif
