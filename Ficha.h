15880d635271a7821594c1045abfb7b87f68cfc0
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2019-07-30 12:39:04 -0500

    Declaracion de la clase Ficha

---
+++ Ficha.h
@@ -0,0 +1,15 @@
+#ifndef FICHA_H
+#define FICHA_H
+
+class Ficha{
+	private:
+		int filaPosicion;
+		int columnaPosicion;
+		bool esDama;
+	public:
+		Ficha();
+		~Ficha();
+		
+		void setPosicion(int fila, int columna);
+		bool puedeComer();
+};
